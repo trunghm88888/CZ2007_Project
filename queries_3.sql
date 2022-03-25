6dea601fa28017a09575a6c9a5086e2a52d50f02
RWKH <russ.wong@hotmail.com>
2022-03-26 01:38:46 +0800

    update Q3

--- queries_3.sql
+++ queries_3.sql
@@ -2,7 +2,7 @@ USE DSS2g3;
 -- QUERIES 3
 
 -- For all products purchased in June 2021 that have been delivered, find the average time from the ordering date to the delivery date. 
-SELECT PO.PName, AVG(DATEDIFF(DAY, O.OrderDateTime, DeliveryDate)) AS AverageDays
+SELECT PO.PName, AVG(DATEDIFF(DAY, O.OrderDateTime, DeliveryDate)) AS Average_Days
 FROM Shiokee.Product_in_order AS PO, Shiokee.[Order] AS O
-WHERE OStatus='delivered' AND DeliveryDate >= DATEADD(DAY, 0, '2021/06/1') AND DeliveryDate <= DATEADD(DAY, 0, '2021/06/30')
+WHERE OStatus='delivered' AND DeliveryDate >= DATEADD(DAY, 0, '2021/06/1') AND DeliveryDate <= DATEADD(DAY, 0, '2021/06/30') AND PO.OrderID = O.OrderID
 GROUP BY PO.PName
