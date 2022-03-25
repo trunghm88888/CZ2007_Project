00f7a733f727d3c42d74230777cc54b983e8031a
RWKH <russ.wong@hotmail.com>
2022-03-26 02:04:47 +0800

    query_6

---
+++ queries_6.sql
@@ -0,0 +1,17 @@
+USE DSS2g3;
+-- QUERIES 6
+
+-- Find shops that made the most revenue in August 2021
+WITH subquery1 AS
+(
+SELECT PO.SName, SUM(OPrice * OQuantity) AS Revenue
+FROM Shiokee.Product_in_order AS PO, Shiokee.[Order] AS O
+WHERE NOT OStatus = 'returned' AND PO.OrderID = O.OrderID AND 
+    O.OrderDateTime >= DATEADD(DAY, 0, '2021/08/1') AND O.OrderDateTime <= DATEADD(DAY, 0, '2021/08/31')
+GROUP BY PO.SName
+)
+
+SELECT q1.SName, Revenue
+FROM subquery1 q1
+GROUP BY q1.SName, Revenue
+ORDER BY Revenue DESC
