234218ddae8c735f01d15d3039bf125a4b7b7b9f
TJJay333 <73398964+TJJay333@users.noreply.github.com>
2022-03-26 22:50:09 +0800

    Update queries_4.sql

--- queries_4.sql
+++ queries_4.sql
@@ -8,3 +8,8 @@ SELECT EID, AVG(CAST(DATEDIFF(DAY, FiledDateTime, HandledDateTime) AS DECIMAL(10
 FROM Shiokee.Complaint
 WHERE EID IS NOT NULL
 GROUP BY EID
+HAVING AVG(CAST(DATEDIFF(DAY, FiledDateTime, HandledDateTime) AS DECIMAL(10,2))) = (Select Min(S1.Latency) From
+	(Select EID, AVG(CAST(DATEDIFF(DAY, FiledDateTime, HandledDateTime) AS DECIMAL(10,2))) AS Latency
+	FROM Shiokee.Complaint
+	WHERE EID IS NOT NULL
+	GROUP BY EID) as S1)
