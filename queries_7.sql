d50657de376b3bfec797e526fc9693e4b334a770
TJJay333 <73398964+TJJay333@users.noreply.github.com>
2022-03-27 16:04:20 +0800

    Update queries_7.sql

--- queries_7.sql
+++ queries_7.sql
@@ -4,20 +4,20 @@ USE DSS2g3;
 -- For users that made the most amount of complaints, find the most expensive products he/she has ever purchased.
 WITH subquery1 AS 
 (
-SELECT UserID, Count(CID) AS TotalComplaints
-FROM Shiokee.Complaint
+SELECT 	UserID, Count(CID) AS TotalComplaints
+FROM 	Shiokee.Complaint
 GROUP BY UserID
-HAVING Count(CID) = (Select Max(X.TotalComplaints) 
-					FROM(	SELECT UserID, Count(CID) AS TotalComplaints
-							FROM Shiokee.Complaint
-							GROUP BY UserID) As X)
+HAVING 	Count(CID) = (SELECT MAX(X.TotalComplaints) FROM(  
+		SELECT UserID, Count(CID) AS TotalComplaints
+		FROM Shiokee.Complaint
+		GROUP BY UserID) As X)
 )
 
-Select PName
-From Shiokee.Product_in_order PIO, Shiokee.[Order] O, subquery1 q1
-WHERE PIO.OrderID = O.OrderID AND O.UserID = q1.UserID 
-	  AND PIO.OPrice = (Select Max(Y.OPrice)
-					   From ( Select PName,OPrice
-							  From Shiokee.Product_in_order PIO, Shiokee.[Order] O, subquery1 q1
-							  WHERE PIO.OrderID = O.OrderID AND O.UserID = q1.UserID ) As Y )
+SELECT	q1.UserID, PName
+FROM 	Shiokee.Product_in_order PIO, Shiokee.[Order] O, subquery1 q1
+WHERE 	PIO.OrderID = O.OrderID AND O.UserID = q1.UserID 
+	AND PIO.OPrice = (SELECT MAX(Y.OPrice) FROM( 
+		SELECT PName,OPrice
+		FROM Shiokee.Product_in_order PIO, Shiokee.[Order] O
+		WHERE PIO.OrderID = O.OrderID AND O.UserID = q1.UserID ) As Y)
 							  
